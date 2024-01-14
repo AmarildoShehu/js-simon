e484960fc6066d4a4d9c442e64d6a504dc1809d1
Amarildo <amarildos@outloo.it>
2024-01-14 23:21:30 +0100

    added my ladder

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log('JS OK');
+
+// MY LADDER
+/*
+1 Funzione per generare numeri casuali
+2 Funzione per visualizzare numeri in pagina
+3 Creare  il timer 30 secondi
+4 Funzione per ottenere l'input dell'utente
+5  Funzione per ottenere l'input dell'utente
+*/
