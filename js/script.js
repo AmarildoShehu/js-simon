fc34a79f374e341d5b26b670b05b6c71d9a3b12d
Amarildo <amarildos@outloo.it>
2024-01-15 15:15:26 +0100

    added function random numbers

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ console.log('JS OK');
 4 Funzione per ottenere l'input dell'utente
 5  Funzione per ottenere l'input dell'utente
 */
+
+
+const numbersContainer = document.getElementById('numbers-container');
+
+
+//##### Funzioni ######
+
+// Funzione per generare numeri casuali
+function generateRandomNumbers() {
+    const numbers = [];
+    for (let i = 0; i < 5; i++) {
+        numbers.push(Math.floor(Math.random() * 10) + 1); // Modifica se vuoi un range diverso
+    }
+    return numbers;
+}
